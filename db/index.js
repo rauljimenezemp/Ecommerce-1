262208f4fe167672189a9f8080191137337b1931
Raúl JR <83794310+rauljimenezemp@users.noreply.github.com>
2021-12-17 18:51:34 -0600

    Agregar SECRETO en llave de entorno y reordenar llamado a dotenv config

--- db/index.js
+++ db/index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectionString = `mongodb+srv://${process.env.MONGODB_DATABASE_USERNAME}:${process.env.MONGODB_DATABASE_PASSWORD}@${process.env.MONGODB_DATABASE_HOSTNAME}/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`
